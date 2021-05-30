3b138c8716dcc1597605b83ea03e10950f3f24c8
Riya Goel <68723881+riyagoel28@users.noreply.github.com>
2021-05-30 11:45:05 +0530

    Eye aspect ratio function done

--- Demo/Eye Detection Test.py
+++ Demo/Eye Detection Test.py
@@ -5,3 +5,28 @@ import pygame               #For playing sound
 import time
 import dlib
 import cv2
+
+#Initialize Pygame and load music
+pygame.mixer.init()
+pygame.mixer.music.load('../Files/alarm.wav')
+
+#Minimum threshold of eye aspect ratio below which alarm is triggerd
+EYE_ASPECT_RATIO_THRESHOLD = 0.3
+time_start=0
+#Minimum consecutive frames for which eye ratio is below threshold for alarm to be triggered
+EYE_ASPECT_RATIO_CONSEC_FRAMES = 100
+
+#COunts no. of consecutuve frames below threshold value
+COUNTER = 0
+
+#Load face cascade which will be used to draw a rectangle around detected faces.
+# face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+
+#This function calculates and return eye aspect ratio
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+
+    ear = (A+B) / (2*C)
+    return ear
