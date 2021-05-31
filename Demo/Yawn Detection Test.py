42d582fcb523f89520fd3f5493886b54d79a4384
Riya Goel <68723881+riyagoel28@users.noreply.github.com>
2021-05-31 12:09:13 +0530

    Yawn Detection Test Successful

--- Demo/Yawn Detection Test.py
+++ Demo/Yawn Detection Test.py
@@ -16,3 +16,67 @@ yawn_timer = 0
 
 #Extract indexes of facial landmarks for mouth
 (Start, End) = face_utils.FACIAL_LANDMARKS_IDXS['mouth']
+
+while True:
+
+    ret, frame = cap.read()  # Reading frames from the video (Web cam)
+    frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+    for face in faces:
+        landmarks = predictor(gray, face)
+        landmarks = face_utils.shape_to_np(landmarks)
+
+        mouth = landmarks[Start:End]
+
+        # Use hull to remove convex contour discrepencies and draw eye shape around eyes
+        mouthHull = cv2.convexHull(mouth)
+        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+
+        # nose = landmarks.parts()[27] ( Finding the center )
+        # cv2.circle(frame, (nose.x, nose.y), 2, (255, 0, 0), 3)
+        # for point in landmarks.parts():
+        #     cv2.circle(frame, (point.x, point.y), 2, (255, 0, 0), 3)
+
+        lip_up = landmarks[62]
+        lip_down = landmarks[66]
+
+        dist = distance.euclidean(lip_up, lip_down)
+        # print(dist)
+
+        if dist > 25:
+
+            # print("Yawn")
+            counter += 1
+            if counter > 100:
+                count_yawn += 1
+                counter = 0
+
+
+        if count_yawn == 0:
+            timer_start = time.time()
+            # print(start2)
+
+        timer_end = time.time() - timer_start
+        # print(timer_end)
+
+        if 1800 < timer_end < 1820:
+            count_yawn = 0
+
+        if count_yawn == 1:
+            mixer.init()
+            mixer.music.load("../Files/alarm.wav")
+            mixer.music.play()
+
+    if ret:
+        cv2.imshow("My Screen", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+print(count_yawn)
