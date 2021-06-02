b774634040663c490eda2171f2128910b434becb
Riya Goel <68723881+riyagoel28@users.noreply.github.com>
2021-06-02 19:38:06 +0530

    Initialized all the variables

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -4,3 +4,20 @@ import cv2
 import dlib
 from pygame import mixer
 import time
+
+#Initialize Pygame and load music
+mixer.init()
+mixer.music.load('Files/alarm.wav')
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("Files/shape_predictor_68_face_landmarks.dat")
+cap = cv2.VideoCapture(0)
+
+#Extract indexes of facial landmarks for mouth
+(Start, End) = face_utils.FACIAL_LANDMARKS_IDXS['mouth']
+
+counter = 0  # Counter to initialize yawn_timer
+count_yawn = 0  # Total number of yawns
+elapsed = 0
+timer_start = 0
+yawn_timer = 0
