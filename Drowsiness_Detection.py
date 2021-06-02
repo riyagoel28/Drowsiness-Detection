50f2fad9bd8d95f2962215d18500d3b2bd81b4a9
Riya Goel <68723881+riyagoel28@users.noreply.github.com>
2021-06-02 19:39:07 +0530

    Eye part Included

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -21,3 +21,25 @@ count_yawn = 0  # Total number of yawns
 elapsed = 0
 timer_start = 0
 yawn_timer = 0
+
+#Extract indexes of facial landmarks for left and right eye
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
+
+#This function calculates and return eye aspect ratio
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+
+    ear = (A+B) / (2*C)
+    return ear
+
+#Minimum threshold of eye aspect ratio below which alarm is triggerd
+EYE_ASPECT_RATIO_THRESHOLD = 0.3
+time_start=0
+#Minimum consecutive frames for which eye ratio is below threshold for alarm to be triggered
+EYE_ASPECT_RATIO_CONSEC_FRAMES = 100
+
+#COunts no. of consecutuve frames below threshold value
+COUNTER = 0
